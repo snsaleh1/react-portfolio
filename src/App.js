c34840b81dffe0f4dfd6bd842981ea9efa594f7b
snsaleh1 <snsaleh11@gmail.com>
2019-11-04 11:57:53 -0600

    changed to kards theme

--- src/App.js
+++ src/App.js
@@ -3,25 +3,24 @@ import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
 import Portfolio from './components/portfolio/portfolio';
-import ServicesCo from './components/servicesCo/servicesCo';
-import Contact from './components/contact/contact';
+import Testimonials from  './components/testimonials/testimonials';
+import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
-
-import './App.css';
-
+import resumeData from './resumeData';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <About />
-        <Resume />
-        <Portfolio />
-        <ServicesCo />
-        <Contact />
-        <Footer />
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
       </div>
     );
   }
 }
+
 export default App;
